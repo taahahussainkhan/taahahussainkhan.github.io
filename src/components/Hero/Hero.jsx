c03f46530cd60b5020620649bb141b484413960b
taahahussainkhan <taaha128@gmail.com>
2024-03-06 14:21:37 +0500

    gh-pages setiup

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Hero.css'
+
+export default function Hero() {
+  return (
+    <div className='container'>
+      <h1>Taaha Hussain Khan here!</h1>
+      <p>Full Stack Developer</p>
+    </div>
+  )
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav className='navbar'>
+            <h1>Taaha Hussain Khan</h1>
+            <ul>
+              <li>Work</li>
+              <li>About</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
