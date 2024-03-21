fb7ddbdc214d6a2b06f62b49bcbe357f5065bb31
L1F21BSCS0917 <L1F21BSCS0917@ucp.com>
2024-03-21 10:59:01 +0500

    Work section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Work from './components/Work/Work'
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Work />
     </div>
   )
 }
---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Work.css'
+
+export default function Navbar() {
+    return (
+        <div className='work'>
+            <h1>Projects</h1>
+            <ul className='projects'>
+                <li>Work</li>
+                <li>About</li>
+            </ul>
+        </div>
+    )
+}
